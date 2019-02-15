082ddd45d02e753dfaa931cfd5eb663a01ac1626
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-02-15 21:03:40 +0200

    Set proxy from back to front

--- server/config.js
+++ server/config.js
@@ -1,3 +1,5 @@
 import '../loaded-env';
 
 export const PORT = process.env.PORT;
+export const WDS_PORT = process.env.WDS_PORT;
+export const SITE_HOST = process.env.SITE_HOST;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+import path from 'path';
 import express from 'express';
+import proxy from 'http-proxy-middleware';
 
-import { PORT } from './config';
+import { PORT, SITE_HOST, WDS_PORT } from './config';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ success: true }));
+app.use(express.static(path.resolve(__dirname, '../static')));
+
+app.use(proxy({ target: `${SITE_HOST}:${WDS_PORT}`, changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
