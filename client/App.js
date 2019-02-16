b2c0d6dbc400dd917f3440544e66b98b2171f75b
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-02-16 17:36:55 +0200

    added material & created auth module

---
+++ client/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { RegisterPage } from './modules/auth';
+
+const App = () => <RegisterPage />;
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import App from './App';
+
 const app = document.querySelector('#app');
 
-render(<h1>App</h1>, app);
+render(<App />, app);
