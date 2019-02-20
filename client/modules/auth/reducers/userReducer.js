0af005e4768af4503a92e0638b84c4fec026caa8
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-02-20 20:51:08 +0200

    Added Admin module with logout button & added logout action

--- client/modules/auth/reducers/userReducer.js
+++ client/modules/auth/reducers/userReducer.js
@@ -1,6 +1,7 @@
 import {
   INITIAL_AUTH_FAIL,
   INITIAL_AUTH_SUCCESS,
+  LOGOUT_SUCCESS,
   REGISTER_USER_FAIL,
   REGISTER_USER_SUCCESS,
 } from '../actions';
@@ -9,12 +10,16 @@ const initialState = {};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+
     case INITIAL_AUTH_SUCCESS:
     case REGISTER_USER_SUCCESS:
       return payload;
+
+    case LOGOUT_SUCCESS:
     case INITIAL_AUTH_FAIL:
     case REGISTER_USER_FAIL:
       return {};
+
     default:
       return state;
   }
