f541a94e0cd07677a06139f470e2d91b309d1406
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-02-25 20:28:21 +0200

    added no-console rule

--- server/index.js
+++ server/index.js
@@ -18,4 +18,4 @@ app.use('/api', apiRoutes);
 
 app.use(proxy({ target: `${SITE_HOST}:${WDS_PORT}`, changeOrigin: true }));
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, () => console.info(`Server is running on port: ${PORT}`));
