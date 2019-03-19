322ddc98d9617f01c2b14ea66d25781a6c3bb4fe
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2019-03-20 00:21:58 +0200

    user creation route

--- core/server/routes/user.js
+++ core/server/routes/user.js
@@ -9,6 +9,7 @@ import {
   withRequiredPasswordField,
   withValidRolesField,
 } from '@core/middlewares/validation';
+import { createUser, serializeUser } from '@core/services/User';
 
 const router = Router();
 
@@ -20,8 +21,18 @@ router.post(
   withRequiredEmailField,
   withNotTakenEmail,
   respondIfError,
-  (req, res) => {
-    return res.json({ success: true });
+  async (req, res) => {
+    const { email, password, roles } = req.body;
+
+    const user = await createUser({
+      email,
+      password,
+      roles,
+    });
+
+    const serializedUser = await serializeUser(user.id);
+
+    return res.json(serializedUser);
   },
 );
 
